ce6dbf16578ce888ac99a67be0aedd99713c2b3c
Kantinan Boonyingsathit <kaseam008@gmail.com>
2023-01-15 01:58:23 +0700

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates FILL_IN, presence: true #replace fill_in with the right code.
+  validates FILL_IN, presence: true #replace fill_in with the right code.
+end
